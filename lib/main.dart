01aaad409c51c8c0d38cd937e2e4431adb103d71
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-22 22:30:09 +0200

    2. Manage State of Level3 Widget depending on (Listening to) the Provider
    Without need to rebuild Level1 & Level2 Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,48 +10,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(data),
+    return Provider<String>(
+      create: (context) => data,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(data),
+          ),
+          body: const Level1(),
         ),
-        body: Level1(data: data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  final String data;
-  const Level1({super.key, required this.data});
+  const Level1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Level2(data: data);
+    return const Level2();
   }
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-  const Level2({super.key, required this.data});
+  const Level2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(),
-        Level3(data: data),
+        const Level3(),
       ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-  const Level3({super.key, required this.data});
+  const Level3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Text(Provider.of<String>(context));
   }
 }
